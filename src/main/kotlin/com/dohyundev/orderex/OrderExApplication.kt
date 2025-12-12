023ed2971393fb16820aeeb1d7f8c8a298678b77
dohyundev0425 <kwondh0351@modaoutlet.co.kr>
2025-12-12 10:25:13 +0900

    first-commit

---
+++ src/main/kotlin/com/dohyundev/orderex/OrderExApplication.kt
@@ -0,0 +1,11 @@
+package com.dohyundev.orderex
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OrderExApplication
+
+fun main(args: Array<String>) {
+    runApplication<OrderExApplication>(*args)
+}
